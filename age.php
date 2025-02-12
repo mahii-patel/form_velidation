5347a7a45d9d7cdc3e0c2489644a897668a04608
mahi <mahivpatel2006@gmail.com>
2025-02-12 13:37:20 +0530

    file age added to remote repository

---
+++ age.php
@@ -0,0 +1,22 @@
+<?php
+     print_r(value: $_POST);
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>form velidation</title>
+</head>
+<body>
+    <h1>This is My form</h1>
+    <form action=""method="POST">
+        Age:<input type="number" name="age" value="">
+        submit:<button>submit</button>
+</body>
+</html>
